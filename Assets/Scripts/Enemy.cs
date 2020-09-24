94e46f8d84c3d4b07bbc0d68ced06d78013e1933
StephenLiu7 <ruiqi.liu7@gmail.com>
2020-09-23 18:02:37 -0700

    EnemyPrototypeTest

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    public float stoppingDistance;
+    public float retreatDistance;
+
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+
+    public GameObject projectile;
+    private Transform player;
+
+    public float dcd;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player").transform;
+
+        timeBtwShots = startTimeBtwShots;
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        dcd = Vector2.Distance(transform.position, player.position);
+        if (Vector2.Distance(transform.position, player.position) > stoppingDistance) {
+
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        }
+        else if (Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > retreatDistance) {
+
+            transform.position = this.transform.position;
+        }
+        else if (Vector2.Distance(transform.position, player.position) < retreatDistance) {
+
+            transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+        }
+
+        if (timeBtwShots <= 0) {
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            timeBtwShots = startTimeBtwShots;
+        }
+        else {
+            timeBtwShots -= Time.deltaTime;
+        }
+    }
+}
