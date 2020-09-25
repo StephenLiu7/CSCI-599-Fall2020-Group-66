fd5b575a8c5837731c26eb44a7e9740e66d832e1
StephenLiu7 <ruiqi.liu7@gmail.com>
2020-09-24 23:54:57 -0700

    Ruiqi_Enemy&Resources
    Enemy movement and shooting with enemy death to be written.

---
+++ Assets/Ruiqi_Works_Resources/Scripts/Ruiqi_Enemy.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ruiqi_Enemy : MonoBehaviour
+{
+    public float speed = 2.0f;
+    public float stoppingDistance;
+    public float retreatDistance;
+    
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+    
+    public GameObject projectile;
+    private Transform player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player").transform;
+        timeBtwShots = startTimeBtwShots;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, player.position) > stoppingDistance) {
+
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        }
+        else if (Vector2.Distance(transform.position, player.position) < stoppingDistance && Vector2.Distance(transform.position, player.position) > retreatDistance) {
+
+            transform.position = this.transform.position;
+        }
+        else if (Vector2.Distance(transform.position, player.position) < retreatDistance) {
+
+            transform.position = Vector2.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+        }
+        
+        if (timeBtwShots <= 0) {
+            Instantiate(projectile, transform.position, Quaternion.identity);
+            timeBtwShots = startTimeBtwShots;
+        }
+        else {
+            timeBtwShots -= Time.deltaTime;
+        }
+    }
+}
---
+++ Assets/Ruiqi_Works_Resources/Scripts/Ruiqi_Enemy_Shot.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ruiqi_Enemy_Shot : MonoBehaviour
+{
+    public float speed;
+
+    private Transform player;
+    private Vector2 target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player").transform;
+        target = new Vector2(player.position.x, player.position.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+        if (transform.position.x == target.x && transform.position.y == target.y) {
+            DestroyProjectTile();
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.CompareTag("Player")) {
+            DestroyProjectTile();
+        }
+    }
+
+    void DestroyProjectTile() {
+        Destroy(gameObject);
+    }
+}
