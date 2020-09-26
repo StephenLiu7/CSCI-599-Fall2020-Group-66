1fe67d995267b22b3afd57d660c31d06ca76c990
Xiaowen <zhuxiaow@usc.edu>
2020-09-25 23:23:38 -0700

    Integrating map

--- Assets/LayerCollisionSetting.cs
+++ Assets/LayerCollisionSetting.cs
@@ -10,6 +10,7 @@ public class LayerCollisionSetting : MonoBehaviour
     private readonly int MONSTER_LAYER = 9;
     private readonly int PLAYER_BULLET_LAYER = 10;
     private readonly int PLAYER_LAYER = 11;
+    private readonly int MAP_LAYER = 20;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,5 +19,7 @@ public class LayerCollisionSetting : MonoBehaviour
         Physics2D.IgnoreLayerCollision(PLAYER_LAYER, PLAYER_BULLET_LAYER);
         Physics2D.IgnoreLayerCollision(PLAYER_BULLET_LAYER, PLAYER_BULLET_LAYER);
         Physics2D.IgnoreLayerCollision(PLAYER_BULLET_LAYER, MONSTER_BULLET_LAYER);
+        Physics2D.IgnoreLayerCollision(MAP_LAYER, PLAYER_BULLET_LAYER);
+        Physics2D.IgnoreLayerCollision(MAP_LAYER, MONSTER_BULLET_LAYER);
     }
 }
