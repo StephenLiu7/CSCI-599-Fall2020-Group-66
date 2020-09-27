6148fd6cb930889349e3f4399ee3f8292959f63f
lmy1108 <lmy961108@gmail.com>
2020-09-26 17:24:22 -0700

    Add Weiyi and Yaqi's monsters

---
+++ Assets/Weiyi_Works/Scripts/Monster_Spawer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Monster_Spawer : MonoBehaviour
+{	
+
+	[SerializeField]
+    private float spawnRadius = 1, time = 0.1f;
+    public Transform[] points;
+    public int totalNumber = 50;
+    public GameObject[] enemies;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnAnEnemy());
+    }
+
+    // Update is called once per frame
+    IEnumerator SpawnAnEnemy()
+    {
+        Vector2 spawnPos = GameObject.Find("Player").transform.position;
+        spawnPos += UnityEngine.Random.insideUnitCircle.normalized * spawnRadius;
+        if(totalNumber > 0)
+        {
+            Instantiate(enemies[UnityEngine.Random.Range(0, enemies.Length)], points[UnityEngine.Random.Range(0, 15)]);
+            totalNumber--;
+        }
+        
+        yield return new WaitForSeconds(time);
+        StartCoroutine(SpawnAnEnemy());
+    }
+}
