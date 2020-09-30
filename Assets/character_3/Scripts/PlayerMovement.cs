8970528889deae00409c3e31c3a004fcb38cc3e6
xiaohai0313 <43129938+xiaohai0313@users.noreply.github.com>
2020-09-29 22:34:51 -0700

    missile model add

--- Assets/character_3/Scripts/PlayerMovement.cs
+++ Assets/character_3/Scripts/PlayerMovement.cs
@@ -19,9 +19,19 @@ public class PlayerMovement : MonoBehaviour
     // =================================================================================================================================================
 
     //============================================= initial Gun & shooting part  =======================================================================
-    private Transform handgun;
+    public Transform handgun;
+    //public Transform player;
+    public Transform cur_gun;
+
     public Transform missile_gun;
-    public GameObject bulletPrefabs;
+    //public GameObject bulletPrefabs;
+    public Transform cur_bullet;
+    public Transform missile;
+    public Transform handgun_bullet;
+
+    double wait_time = 0.4;
+
+
     public float proTime = 0.0f;
     public float NextTime = 0.0f;
     bool LOR = false;       // initial facing right
@@ -30,8 +40,23 @@ public class PlayerMovement : MonoBehaviour
 
     private void Awake()
     {
-        handgun = transform.Find("handgun");
-        missile_gun = handgun = GameObject.Find("missile_gun").transform;
+        //missile.gameObject.setActive(false);
+        missile_gun.gameObject.GetComponent<Renderer>().enabled = false;
+        // set in the Unity those are backup code
+        //Destroy(missile_gun.gameObject);
+        //player = transform.Find("Player");
+        //player = transform.Find("Player");
+        //handgun = transform.Find("handgun");
+
+        //handgun = GameObject.Find("handgun").transform;
+        //missile_gun = handgun = GameObject.Find("missile_gun").transform;
+        //cur_gun = handgun;
+
+        //missile = GameObject.Find("player_missle").transform;
+        //handgun_bullet = handgun = GameObject.Find("player_bullet").transform;
+        //cur_bullet = handgun_bullet;
+        //handgun = transform.Find("handgun");
+        //missile_gun = handgun = GameObject.Find("missile_gun").transform;
     }
 
     void Update()
@@ -47,7 +72,7 @@ public class PlayerMovement : MonoBehaviour
         }
         moveDirec = new Vector2(movement.x, movement.y).normalized;
         */
-        Vector3 theScale = handgun.localScale;
+        Vector3 theScale = cur_gun.localScale;
          
         if(movement.x > 0.001f)
         {
@@ -55,9 +80,9 @@ public class PlayerMovement : MonoBehaviour
             {
                 LOR = false;
                 theScale.x *= -1;
-                handgun.localScale = theScale;
+                cur_gun.localScale = theScale;
                 Vector2 diff = new Vector2(0.25f, 0);
-                handgun.Translate(diff,Space.World);        // space world == absolute axis    Didn't change with object rotate
+                cur_gun.Translate(diff,Space.World);        // space world == absolute axis    Didn't change with object rotate
             }
             facing = 1.0f;
         }
@@ -67,9 +92,9 @@ public class PlayerMovement : MonoBehaviour
             {
                 LOR = true;
                 theScale.x *= -1;
-                handgun.localScale = theScale;
+                cur_gun.localScale = theScale;
                 Vector2 diff = new Vector2(-0.25f, 0);
-                handgun.Translate(diff, Space.World);
+                cur_gun.Translate(diff, Space.World);
             }
             facing = -1.0f;
         }
@@ -83,7 +108,33 @@ public class PlayerMovement : MonoBehaviour
         // ================================================================ shooting time SEPA =============================================================
         proTime = Time.fixedTime;
         FollowMouseRotate();
-        if (proTime - NextTime >= 0.2)
+        
+        // test model for switch weapon
+        if (Input.GetMouseButtonDown(1))
+        {
+            if(cur_bullet == missile)
+            {
+                wait_time = 0.5;
+                missile_gun.gameObject.GetComponent<Renderer>().enabled = false;
+                handgun.gameObject.GetComponent<Renderer>().enabled = true;
+                cur_bullet = handgun_bullet;
+                cur_gun = handgun;
+                //cur_gun = Instantiate(handgun, transform.position, Quaternion.identity);
+                //Destroy(missile_gun.gameObject);
+            }
+            else
+            {
+                wait_time = 1.2;
+                missile_gun.gameObject.GetComponent<Renderer>().enabled = true;
+                handgun.gameObject.GetComponent<Renderer>().enabled = false;
+                cur_bullet = missile;
+                cur_gun = missile_gun;
+                //cur_gun = Instantiate(missile_gun, transform.position, Quaternion.identity);
+                //Destroy(handgun.gameObject);
+            }
+        }
+        Debug.Log(wait_time);
+        if (proTime - NextTime >= wait_time)
         {
             
             Shooting();
@@ -106,7 +157,7 @@ public class PlayerMovement : MonoBehaviour
     {
         Vector3 mouse = Input.mousePosition;
         //Debug.Log(mouse);
-        Vector3 obj = Camera.main.WorldToScreenPoint(handgun.position);
+        Vector3 obj = Camera.main.WorldToScreenPoint(cur_gun.position);
         Vector3 direction = obj - mouse;
         if (LOR == true)
         { direction = obj - mouse; }
@@ -114,7 +165,7 @@ public class PlayerMovement : MonoBehaviour
         direction.z = 0f;
         //Debug.Log(direction);
         direction = direction.normalized;
-        handgun.right = direction;
+        cur_gun.right = direction;
         //  Debug.Log(player.GetAxis("aimHor"));
 
         //transform.right = direction;         if rotate with Player together
@@ -125,14 +176,17 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetMouseButtonDown(0) || Input.GetMouseButton(0))
         {
             Vector3 bulletDirection = Input.mousePosition;
-            Vector3 obj = Camera.main.WorldToScreenPoint(handgun.position);
+            Vector3 obj = Camera.main.WorldToScreenPoint(cur_gun.position);
             Vector3 direction = bulletDirection - obj;
             direction.Normalize();
-            GameObject bullet = Instantiate(bulletPrefabs, transform.position, Quaternion.identity);
+            Transform bullet = Instantiate(cur_bullet, transform.position, Quaternion.identity);
             //bullet.right = bulletDirection;
-            bullet.GetComponent<Rigidbody2D>().velocity = direction * 11;
+            if (bullet == missile)
+            { bullet.GetComponent<Rigidbody2D>().velocity = direction * 11; }
+            else { bullet.GetComponent<Rigidbody2D>().velocity = direction * 5; }
+            //bullet.GetComponent<Rigidbody2D>().velocity = direction * 11;
             bullet.transform.Rotate(0.0f, 0.0f, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg);
-            Destroy(bullet, 10.0f);
+            Destroy(bullet.gameObject, 10.0f);
         }
     }
     // =================================================================================================================================================
