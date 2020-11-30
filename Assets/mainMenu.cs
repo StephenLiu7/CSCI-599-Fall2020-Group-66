448a740b22d19a25c7953ff082f11d7b9c0a858c
AlanZhu <37814439+AlannnZzz@users.noreply.github.com>
2020-11-30 23:56:38 +0800

    game mode choose

--- Assets/mainMenu.cs
+++ Assets/mainMenu.cs
@@ -8,9 +8,37 @@ public class mainMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
-   
-     public void QuitGame()
+    /*
+    public void SettingGame()
+    {
+        SceneManager.LoadScene("gameModeMenu");
+    }
+    */
+
+    public void QuitGame()
     {
         Application.Quit();
     }
+
+    public void easyMode()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void mediumMode()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+
+    public void hardMode()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+
+    public void backToMainMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+
 }
