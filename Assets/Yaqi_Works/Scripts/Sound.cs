08a9a32da61ac478f8cd088fb4a6331c97c6c25c
YaqiHan <hanyaqi141@gmail.com>
2020-12-05 21:42:01 -0800

    Add Blood Potion Sound and Change Gun Sound

---
+++ Assets/Yaqi_Works/Scripts/Sound.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound : MonoBehaviour
+{
+    public AudioSource bloodSound;
+    public AudioSource changeGunSound1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void playSoundEffect()
+    {
+        bloodSound.Play();
+    }
+
+    public void playSoundEffect1()
+    {
+        changeGunSound1.Play();
+    }
+
+}
